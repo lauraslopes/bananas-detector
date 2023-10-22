7d64988b3cead19b7ce0c2dce5de16aeb0ba1085
Laura Lopes <lauraslopes94@gmail.com>
2023-10-22 20:48:52 -0300

    Refactoring Detector class

--- detector.py
+++ detector.py
@@ -1,23 +1,33 @@
 from ultralytics import YOLO
 
+
 class Detector:
+    def __init__(self, weights, device):
+        self.model = YOLO(weights)
+        self.device = device
 
-    def __init__():
-        pass
+    def train(self, dataset, epochs, batch_size):
+        self.model.train(
+            data=dataset,
+            epochs=epochs,
+            batch=batch_size,
+            device=self.device,
+            degrees=45,
+            flipud=1,
+            mixup=1,
+        )
 
-    def train(weights, dataset, epochs, batch_size, device):
-
-        model = YOLO(weights)
-        model.train(data=dataset, epochs=epochs, batch=batch_size, device=device, degrees=45, flipud=1, mixup=1)
-
-        return model
-
-    def inference(weights, source, conf, device):
-        model = YOLO(weights)
+    def inference(self, source, conf):
         # predict on an image
-        return model.predict(source, conf=conf, device=device, save=True, save_txt=True, save_conf=True)
+        return self.model.predict(
+            source,
+            conf=conf,
+            device=self.device,
+            save=True,
+            save_txt=True,
+            save_conf=True,
+        )
 
-    def get_metrics(model):
+    def get_metrics(self):
         # evaluate model performance on the validation set
-        return model.val()
-
+        return self.model.val()
--- main.py
+++ main.py
@@ -2,29 +2,30 @@ from detector import Detector
 import cv2
 import argparse
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov8m.pt', help='model.pt path(s)')
-    parser.add_argument('--dataset', type=str, default='data/data.yaml', help='dataset.yaml path(s)')
-    parser.add_argument('--epochs', type=int, default=100, help='number of traning epochs')
-    parser.add_argument('--batch-size', type=int, default=16, help='batch size')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--train', action='store_true', help='whether is training or not')
-    parser.add_argument('--show-results', action='store_true', help='show results images with predictions')
-    parser.add_argument('--source', type=str, default='', help='source for inference (path to an image or directory of images)')
-    parser.add_argument('--conf', type=float, default=0.25, help='object confidence threshold for detection')
+    parser.add_argument("--weights", type=str, default="yolov8m.pt", help="model.pt path(s)")
+    parser.add_argument("--dataset", type=str, default="data/data.yaml", help="dataset.yaml path(s)")
+    parser.add_argument("--epochs", type=int, default=100, help="number of traning epochs")
+    parser.add_argument("--batch-size", type=int, default=16, help="batch size")
+    parser.add_argument("--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu")
+    parser.add_argument("--train", action="store_true", help="whether is training or not")
+    parser.add_argument("--show-results", action="store_true", help="show results images with predictions")
+    parser.add_argument("--source", type=str, default="", help="source for inference (path to an image or directory of images)")
+    parser.add_argument("--conf", type=float, default=0.25, help="object confidence threshold for detection")
     opt = parser.parse_args()
     print(opt)
 
+    detector = Detector(opt.weights, opt.device)
     if opt.train:
-        model = Detector.train(opt.weights, opt.dataset, opt.epochs, opt.batch_size, opt.device)
-        print(Detector.get_metrics(model))
+        detector.train(opt.dataset, opt.epochs, opt.batch_size)
+        print(detector.get_metrics())
     else:
-        results = Detector.inference(opt.weights, opt.source, opt.conf, opt.device)
+        results = detector.inference(opt.source, opt.conf)
 
         if opt.show_results:
             # Show the results
             for i, r in enumerate(results):
                 im_array = r.plot()  # plot a BGR numpy array of predictions
-                cv2.imshow('Result', im_array)
-                cv2.waitKey(0) 
+                cv2.imshow("Result", im_array)
+                cv2.waitKey(0)
