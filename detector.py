09055bd6a431c7b8a2cd4b46bd63f0eab7e33b83
lauraslopes <lauraslopes94@gmail.com>
2023-10-22 19:53:49 -0300

    Inference

--- detector.py
+++ detector.py
@@ -8,13 +8,16 @@ class Detector:
     def train(weights, dataset, epochs, batch_size, device):
 
         model = YOLO(weights)
-        model.train(data=dataset, epochs=epochs, batch=batch_size, device=device)
+        model.train(data=dataset, epochs=epochs, batch=batch_size, device=device, degrees=45, flipud=1, mixup=1)
 
         return model
 
-    def inference():
-        pass
+    def inference(weights, source, conf, device):
+        model = YOLO(weights)
+        # predict on an image
+        return model.predict(source, conf=conf, device=device, save=True, save_txt=True, save_conf=True)
 
     def get_metrics(model):
         # evaluate model performance on the validation set
         return model.val()
+
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from detector import Detector
+from PIL import Image
 import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov8m.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', type=str, default='yolov8m.pt', help='model.pt path(s)')
     parser.add_argument('--dataset', type=str, default='data/data.yaml', help='dataset.yaml path(s)')
     parser.add_argument('--epochs', type=int, default=100, help='number of traning epochs')
     parser.add_argument('--batch-size', type=int, default=16, help='batch size')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--train', action='store_true', help='whether is training or not')
+    parser.add_argument('--show-results', action='store_true', help='show results images with predictions')
+    parser.add_argument('--source', type=str, default='', help='source for inference (path to an image or directory of images)')
+    parser.add_argument('--conf', type=float, default=0.25, help='object confidence threshold for detection')
     opt = parser.parse_args()
     print(opt)
 
     if opt.train:
         model = Detector.train(opt.weights, opt.dataset, opt.epochs, opt.batch_size, opt.device)
         print(Detector.get_metrics(model))
+    else:
+        results = Detector.inference(opt.weights, opt.source, opt.conf, opt.device)
+
+        if opt.show_results:
+            # Show the results
+            for i, r in enumerate(results):
+                im_array = r.plot()  # plot a BGR numpy array of predictions
+                im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+                im.show()  # show image
+                im.save('results'+str(i)+'.jpg')  # save image
